ace5a9142e659b3f18c7469b6e8e0b0f9b4c87df
maiconlara <maiconluan.lara@yahoo.com.br>
2023-08-16 15:39:52 -0300

    Adding states and mean function

--- aula03-media/src/components/Form/FormInput/index.tsx
+++ aula03-media/src/components/Form/FormInput/index.tsx
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { TextInput, View, Text } from "react-native";
 
 import { styles } from "./styles";
 
 interface FormInputProps {
   text: string;
+  handleNumber: (newNumber: number) => void;
 }
 
-export function FormInput({ text }: FormInputProps) {
+export function FormInput({ text, handleNumber }: FormInputProps) {
   return (
     <View style={styles.container}>
       <View style={styles.containerDiv}>
@@ -15,6 +16,7 @@ export function FormInput({ text }: FormInputProps) {
         <TextInput
           style={styles.textInput}
           keyboardType="number-pad"
+          onChangeText={(event) => handleNumber(parseInt(event))}
         ></TextInput>
       </View>
     </View>
--- aula03-media/src/components/Form/index.tsx
+++ aula03-media/src/components/Form/index.tsx
@@ -1,21 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text, View, Button, Alert } from "react-native";
 import { FormInput } from "./FormInput";
 
 import { styles } from "./styles";
 
 export function Form() {
+  const [firstNumber, setFirstNumber] = useState<number>(0);
+  const [secondNumber, setSecondNumber] = useState<number>(0);
+  const mean = (firstNumber + secondNumber) / 2;
   return (
     <View style={styles.container}>
       <Text style={styles.textTitle}>Calculadora de Média</Text>
-      <FormInput text="Primeiro" />
-      <FormInput text="Segundo" />
+      <FormInput text="Primeiro" handleNumber={setFirstNumber}/>
+      <FormInput text="Segundo" handleNumber={setSecondNumber}/>
 
       <View style={styles.containerDiv}>
         <Button
           title="Calcular"
           color="#5ED3F3"
-          onPress={() => Alert.alert("Sem alerta papai")}
+          onPress={() => Alert.alert(`A média é ${mean}`)}
         ></Button>
       </View>
     </View>
